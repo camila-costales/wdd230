f386e91cd8f60d20c75596fcaf3c2dc2ef7ae460
camila-costales <114012885+camila-costales@users.noreply.github.com>
2023-06-15 10:26:15 -0600

    discover

--- Lesson7 Chamber/scripts/script.js
+++ Lesson7 Chamber/scripts/script.js
@@ -1,5 +1,7 @@
 const hamButton = document.querySelector('#hamButton');
 const navigation = document.querySelector('.navigation');
+const visitsDisplay = document.querySelector(".last-visit");
+
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('responsive');
@@ -47,3 +49,63 @@ if (currentDate.getDay() === 1 || currentDate.getDay() === 2) {
 
 
 
+// images discover 
+// Get all images to be replaced
+const imagesToLoad = document.querySelectorAll("[data-src]");
+
+// function to load the actual image
+const loadImages = (img) => {
+  img.setAttribute("src", img.getAttribute("data-src"));
+  img.onload = () => {
+    img.removeAttribute("data-src");
+  };
+};
+
+const imageOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imageOptions);
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+// discover last visit
+
+
+let currentVisit = Date.now();
+
+let lastVisit = Number(window.localStorage.getItem("last-visit-ls"));
+
+let lastVisitDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(new Date(lastVisit));
+
+let timeSinceLast = Math.round((currentVisit - lastVisit) / (86400000));
+
+if (lastVisit !== 0) {
+  visitsDisplay.textContent = "The last time you visited this page was "+lastVisitDate+", which was "+timeSinceLast+" days ago. Welcome back!";
+} else {
+  visitsDisplay.textContent = "This is your first time visiting this page!"
+};
+
+localStorage.setItem("last-visit-ls", currentVisit)
+
+function selectResponse() {
+	const s = document.querySelector('#selected');
+	const sel = document.querySelector('#select-mem');
+	s.style.display = "block";
+	s.textContent = sel.value;
+};

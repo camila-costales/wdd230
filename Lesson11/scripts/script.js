80427b069d7a765b461a33454194e3d4f4c2ee60
camila-costales <114012885+camila-costales@users.noreply.github.com>
2023-06-29 18:01:23 -0600

    js error

--- Lesson11/scripts/script.js
+++ Lesson11/scripts/script.js
@@ -96,9 +96,9 @@ let lastVisitDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format
 let timeSinceLast = Math.round((currentVisit - lastVisit) / (86400000));
 
 if (lastVisit !== 0) {
-  visitsDisplay.textContent = "The last time you visited this page was "+lastVisitDate+", which was "+timeSinceLast+" days ago. Welcome back!";
+  visitsDisplay.innerHTML = "The last time you visited this page was "+lastVisitDate+", which was "+timeSinceLast+" days ago. Welcome back!";
 } else {
-  visitsDisplay.textContent = "This is your first time visiting this page!"
+  visitsDisplay.innerHTML = "This is your first time visiting this page!"
 };
 
 localStorage.setItem("last-visit-ls", currentVisit)

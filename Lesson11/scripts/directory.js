7067a12d1bdf027b33e3cc78009489738a934298
camila-costales <114012885+camila-costales@users.noreply.github.com>
2023-06-29 18:08:28 -0600

    java script error

--- Lesson11/scripts/directory.js
+++ Lesson11/scripts/directory.js
@@ -65,3 +65,29 @@ listButton.addEventListener('click', () => {
     display.classList.add('list');
     display.classList.remove('grid');
 });
+
+// discover last visit
+
+
+let currentVisit = Date.now();
+
+let lastVisit = Number(window.localStorage.getItem("last-visit-ls"));
+
+let lastVisitDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(new Date(lastVisit));
+
+let timeSinceLast = Math.round((currentVisit - lastVisit) / (86400000));
+
+if (lastVisit !== 0) {
+  visitsDisplay.innerHTML = "The last time you visited this page was "+lastVisitDate+", which was "+timeSinceLast+" days ago. Welcome back!";
+} else {
+  visitsDisplay.innerHTML = "This is your first time visiting this page!"
+};
+
+localStorage.setItem("last-visit-ls", currentVisit)
+
+function selectResponse() {
+	const s = document.querySelector('#selected');
+	const sel = document.querySelector('#select-mem');
+	s.style.display = "block";
+	s.textContent = sel.value;
+};

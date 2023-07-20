0b9880bec9c480b9b8a0549e07fe94e805956d00
camila-costales <114012885+camila-costales@users.noreply.github.com>
2023-07-20 01:28:01 -0600

    updates

--- Lesson14/scripts/drinksordered.js
+++ Lesson14/scripts/drinksordered.js
@@ -1,12 +1,8 @@
-const drinksOrdered = document.querySelector("#drinksOrdered");
 
-const totOrdersLS = window.localStorage.getItem("totOrdersLs");
-
-if (totOrdersLS === null) {
-    window.localStorage.setItem("totOrdersLs", 0);
-    let totOrders = Number(totOrdersLS);
-    drinksOrdered.textContent = totOrders;
-} else {
-    let totOrders = Number(totOrdersLS);
-    drinksOrdered.textContent = totOrders;
-};
+const totalDrinks = document.querySelector("#drinksordered");
+let numDrinks = Number(window.localStorage.getItem("drinks-ls"));
+    if (numDrinks !== 0) {
+        totalDrinks.textContent = numDrinks;
+    } else {
+        totalDrinks.textContent = `No drinks have been ordered`;
+    }

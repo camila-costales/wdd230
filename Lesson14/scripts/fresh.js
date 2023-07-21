890b59bebde23a2aa247617e41e895dafd3a1849
camila-costales <114012885+camila-costales@users.noreply.github.com>
2023-07-20 20:40:38 -0600

    last

--- Lesson14/scripts/fresh.js
+++ Lesson14/scripts/fresh.js
@@ -1,4 +1,119 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json'
+
+
+
+
+    // Fetch fruit data from JSON source
+    fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not OK');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data); // Log the response to inspect the JSON data structure
+
+        if (!data || !Array.isArray(data)) {
+            throw new Error('Invalid JSON data format');
+        }
+
+        const fruitOptions = data.map(fruit => fruit.name);
+
+        // Function to populate select elements with fruit options
+        const populateSelect = (selectElement, options) => {
+            const select = document.getElementById(selectElement);
+
+            // Loop through options and create option elements
+            options.forEach(option => {
+                const optionElement = document.createElement('option');
+                optionElement.value = option;
+                optionElement.textContent = option;
+                select.appendChild(optionElement);
+            });
+        };
+
+        // Call the populateSelect function for each select element
+        populateSelect('fruit1', fruitOptions);
+        populateSelect('fruit2', fruitOptions);
+        populateSelect('fruit3', fruitOptions);
+    })
+    .catch(error => console.error('Error fetching fruit data:', error));
+
+
+    
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Function to calculate the total nutrients based on the selected fruits
+  const calculateTotalNutrients = () => {
+      const fruit1 = document.getElementById('fruit1').value;
+      const fruit2 = document.getElementById('fruit2').value;
+      const fruit3 = document.getElementById('fruit3').value;
+
+      // Get the fruit data from the JSON source
+      fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+          .then(response => response.json())
+          .then(data => {
+              const selectedFruits = [fruit1, fruit2, fruit3];
+
+              let totalCarbohydrates = 0;
+              let totalProtein = 0;
+              let totalFat = 0;
+              let totalSugar = 0;
+              let totalCalories = 0;
+
+              data.forEach(fruit => {
+                  if (selectedFruits.includes(fruit.name)) {
+                      totalCarbohydrates += fruit.nutritions.carbohydrates;
+                      totalProtein += fruit.nutritions.protein;
+                      totalFat += fruit.nutritions.fat;
+                      totalSugar += fruit.nutritions.sugar;
+                      totalCalories += fruit.nutritions.calories;
+                  }
+              });
+
+              // Get user input values
+              const firstName = document.getElementById('first-name').value;
+              const email = document.getElementById('email').value;
+              const phone = document.getElementById('phone').value;
+              const specialInstructions = document.getElementsByName('userText')[0].value;
+
+              // Format the output and display it in the "submit-message" div
+              const outputArea = document.getElementById('submit-message');
+              outputArea.innerHTML = `
+                  <h2>Your Order Summary</h2>
+                  <p><strong>Name:</strong> ${firstName}</p>
+                  <p><strong>Email:</strong> ${email}</p>
+                  <p><strong>Phone Number:</strong> ${phone}</p>
+                  <p><strong>Selected Fruits:</strong> ${selectedFruits.join(', ')}</p>
+                  <p><strong>Special Instructions:</strong> ${specialInstructions}</p>
+                  <h3>Total Nutrients:</h3>
+                  <p><strong>Total Carbohydrates:</strong> ${totalCarbohydrates.toFixed(2)} g</p>
+                  <p><strong>Total Protein:</strong> ${totalProtein.toFixed(2)} g</p>
+                  <p><strong>Total Fat:</strong> ${totalFat.toFixed(2)} g</p>
+                  <p><strong>Total Sugar:</strong> ${totalSugar.toFixed(2)} g</p>
+                  <p><strong>Total Calories:</strong> ${totalCalories}</p>
+              `;
+          })
+          .catch(error => console.error('Error fetching fruit data:', error));
+  };
+
+  // Add event listener for form submission
+  const orderForm = document.getElementById('drink-form');
+  orderForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission to a new page
+      calculateTotalNutrients(); // Calculate and display the order summary
+  });
+});
+
+
+
+
+
+
+    
+
+
+/* const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json'
 
 const getFruits = async()=>{
     try{
@@ -35,7 +150,7 @@ function displayResults(data) {
 
     fruitsFieldset.appendChild(checkbox);
     fruitsFieldset.appendChild(label);
-  }
+  } 
 
 function selectedFruits() {
   const checkboxes = fruitsFieldset.querySelectorAll('input[type="checkbox"]');
@@ -122,4 +237,4 @@ function selectedFruits() {
     });
   
 }
-};
+};*/
